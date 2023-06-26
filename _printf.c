3928cc99b82125c823b771313180b2a97256a133
Katalyst99 <Katalyst99>
2023-06-26 16:21:15 -0700

    Code fix

--- _printf.c
+++ _printf.c
@@ -12,7 +12,6 @@ int _printf(const char *format, ...)
 	int i = 0, c = 0;
 	va_list ap;
 	int (*fnc)(va_list);
-	char s;
 
 	va_start(ap, format);
 	if (format == NULL)
@@ -21,34 +20,32 @@ int _printf(const char *format, ...)
 	{
 		if (format[i] == '%')
 		{
-			s = format[i + 1];
-			fnc = spec_check(s);
+			if (format[i + 1] == '\0')
+				return (-1);
+			fnc = spec_check(format[i + 1]);
 			if (fnc != NULL)
 			{
 				c += fnc(ap);
 				i += 2;
 				continue;
 			}
-			else if (s != '\0')
+			else if (format[i + 1] != '\0')
 			{
-				_putchar(s);
+				_putchar(format[i + 1]);
 				c++;
 				i++;
 				continue;
 			}
 			else
 				break;
-		}	else
-			{
-				_putchar(format[i]);
-				c++;
-				i++;
-				continue;
-			}
 		}
-		if (format[i] != '\0')
-			return (-1);
+		else
+		{
+			_putchar(format[i]);
+			c++;
+			i++;
+			continue;
 		}
-		va_end(ap);
-		return (c);
 	}
+	va_end(ap);
+	return (c);
